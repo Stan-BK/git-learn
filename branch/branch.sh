ac4f23b6d0581131a95980df12374f4c4991ab1e
Stan-BK <2568143457@qq.com>
2022-05-14 17:19:34 +0800

    feat

---
+++ branch/branch.sh
@@ -0,0 +1,18 @@
+# 解开注释后运行已查看效果
+
+# 查看本地分支
+# git branch
+# 查看所有分支
+# git branch -a
+
+# 新建分支
+# git branch new-branch
+# 删除分支
+# git branch -d new-branch
+
+# 切换分支
+# git checkout new-branch
+# git switch new-branch
+
+# 合并分支
+# git merge new-branch
